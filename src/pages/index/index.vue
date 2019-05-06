240aeb1bcd9e48d412a703479ca0f0475a94860f
sasonglin <sasonglinzb@163.com>
2019-05-06 11:09:31 +0800

    首页布局完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,125 +1,201 @@
 <template>
-  <div @click="clickHandle">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
+  <div>
+    <!-- 搜索 -->
+    <div class="search">
+      <!-- 搜索框 -->
+      <div class="input-box"
+           placeholder="请输入商品">
+        <input type="text">
       </div>
+      <!-- 搜索结果 -->
+      <div class="result"></div>
     </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
+    <!-- 焦点图 -->
+    <swiper class="banner"
+            indicator-dots
+            autoplay
+            circular
+            indicator-color="rgba(255,255,255,.6)"
+            indicator-active-color="#fff">
+      <swiper-item>
+        <navigator url="">
+          <image src="/static/uploads/banner1.png">
+          </image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator url="">
+          <image src="/static/uploads/banner2.png">
+          </image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator url="">
+          <image src="/static/uploads/banner3.png">
+          </image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+    <!-- 导航 -->
+    <div class="navs">
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_1@2x.png">
+        </image>
+      </navigator>
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_2@2x.png">
+        </image>
+      </navigator>
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_3@2x.png">
+        </image>
+      </navigator>
+      <navigator url="">
+        <image src="/static/uploads/icon_index_nav_4@2x.png">
+        </image>
+      </navigator>
+    </div>
+    <!-- 楼层 -->
+    <div class="floors">
+      <div class="floor">
+        <div class="title">
+          <image src="/static/uploads/pic_floor02_title.png">
+          </image>
+        </div>
+        <div class="pics">
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_1@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_2@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_3@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_4@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_5@2x.png">
+            </image>
+          </navigator>
+        </div>
       </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-
-    <div class="all">
-        <div class="left">
+      <div class="floor">
+        <div class="title">
+          <image src="/static/uploads/pic_floor02_title.png">
+          </image>
         </div>
-        <div class="right">
+        <div class="pics">
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_1@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_2@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_3@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_4@2x.png">
+            </image>
+          </navigator>
+          <navigator url="">
+            <image src="/static/uploads/pic_floor02_5@2x.png">
+            </image>
+          </navigator>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
-
 export default {
-  data () {
-    return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
-  },
 
-  components: {
-    card
-  },
-
-  methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
-  },
-
-  created () {
-    // let app = getApp()
-  }
 }
 </script>
 
 <style scoped>
-.userinfo {
+/* 搜索 */
+.search {
+}
+.search .input-box {
+  background-color: #ee4451;
+  padding: 20rpx 30rpx;
+}
+.search .input-box input {
+  height: 75rpx;
+  background-color: #fff;
+  border-radius: 10rpx;
+  padding-left: 15rpx;
+  font-size: 27rpx;
+  color: #666;
+}
+
+/* 焦点图 */
+.banner {
+  width: 750rpx;
+  height: 340rpx;
+}
+.banner navigator {
+  width: 100%;
+  height: 100%;
+}
+
+/* 导航 */
+.navs {
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: space-around;
+  padding-top: 30rpx;
 }
-
-.userinfo-avatar {
+.navs navigator {
   width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
+  height: 140rpx;
 }
 
-.userinfo-nickname {
-  color: #aaa;
+/* 楼层 */
+.floors {
 }
-
-.usermotto {
-  margin-top: 150px;
+.floors .title {
+  width: 750rpx;
+  height: 59rpx;
+  padding-top: 30rpx;
+  padding-left: 15rpx;
+  background: #f4f4f4;
 }
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
+.floors .pics {
+  padding: 20rpx 18rpx;
+  overflow: hidden;
 }
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
+.floors .pics navigator {
+  height: 188rpx;
+  margin-left: 10rpx;
+  margin-bottom: 10rpx;
+  float: left;
 }
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
+.floors .pics navigator:first-child {
+  width: 232rpx;
+  height: 386rpx;
+  margin-left: 0;
 }
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
+.floors .pics navigator:nth-child(2) {
+  width: 273rpx;
 }
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
+.floors .pics navigator:nth-child(3) {
+  width: 193rpx;
+}
+.floors .pics navigator:nth-child(4) {
+  width: 193rpx;
+}
+.floors .pics navigator:last-child {
+  width: 273rpx;
 }
 </style>
